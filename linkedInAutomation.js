c7cc65b964b0530cd0a84ff6eb77e938725ef9b0
Sakshi Srivastava <104548326+sakshi-279@users.noreply.github.com>
2023-01-18 17:34:28 +0530

    Create linkedInAutomation.js

---
+++ linkedInAutomation.js
@@ -0,0 +1,5 @@
+var request_send = document
+var request_send = document.getElementsByClassName("full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view")
+for (var i=0; i<8; i++){
+    request_send[i].click()
+}
